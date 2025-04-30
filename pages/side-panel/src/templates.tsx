922400d9e4967eb600c11d739470f507b17695e9
Thomas Cornelius <thomas@graph8.com>
2025-04-29 21:15:30 -0400

    css

--- pages/side-panel/src/templates.tsx
+++ pages/side-panel/src/templates.tsx
@@ -17,4 +17,30 @@ export const defaultTemplates = [
     content:
       'Visit our help center at https://help.graph8.com for tutorials, documentation, and answers to frequently asked questions.',
   },
+  {
+    id: '4',
+    title: 'Get LinkedIn Post',
+    content: 'Get the last post content for the following user https://www.linkedin.com/in/thomasjcornelius/',
+  },
+  {
+    id: '5',
+    title: 'Get Latest Tweet',
+    content: 'Get the last tweet from the following user https://x.com/graph8tc',
+  },
+  {
+    id: '6',
+    title: 'Get LinkedIn Followers',
+    content: 'Get last 5 new followers for https://www.linkedin.com/company/102481684/admin/analytics/followers/',
+  },
+  {
+    id: '7',
+    title: 'Get Post Commenters',
+    content: 'Get all people that commented on latest post from https://www.linkedin.com/in/davidkdulany/',
+  },
+  {
+    id: '8',
+    title: 'Get Producthunt Upvotes',
+    content:
+      'Get all profile URLs for comments on producthunt campaign https://www.producthunt.com/products/clay-6#clay-2-0',
+  },
 ];
